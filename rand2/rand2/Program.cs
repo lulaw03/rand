581b0b66810eed7d81cafc98ed3648ebb815d817
m305-22\w401_ <lulaw03@gmail.com>
2018-04-12 16:54:31 +0800

    產生 五列 六個變數 數列

--- rand2/rand2/Program.cs
+++ rand2/rand2/Program.cs
@@ -11,9 +11,15 @@ namespace rand2
         static void Main(string[] args)
         {
             Random rand = new Random();// 宣告變數
-            for (int i = 0; i < 6;i++ ) 
+            for (int i = 0; i < 7;i++ ) 
             {
-                Console.WriteLine(rand.Next(1, 43));// 亂數範圍為1到43，但可能會重複
+                int r0 = rand.Next(1, 43);
+                int r1 = rand.Next(1, 43);
+                int r2 = rand.Next(1, 43);
+                int r3 = rand.Next(1, 43);
+                int r4 = rand.Next(1, 43);
+
+                Console.WriteLine("{0:00}{1:00}{2:00}{3:00}{4:00}",r0,r1,r2,r3,r4);// 亂數範圍為1到43，但可能會重複
             }
             Console.ReadKey();// 停止畫面
         }
