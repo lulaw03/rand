2a3c5c36eaf7eea88eb072ad16d949e8586a8365
m305-22\w401_ <lulaw03@gmail.com>
2018-04-12 17:33:16 +0800

    不重號的樂透6成功版

--- rand2/rand2/Program.cs
+++ rand2/rand2/Program.cs
@@ -35,15 +35,13 @@ namespace rand2
             Random rand1 = new Random();
             for (int h = 1; h <= 6; h++ )
             {
-                int r = rand.Next(1, 43);
+                int r = rand.Next(1, numbers.Count);
                 Console.WriteLine(numbers[r]);
+
             // 步湊三將其數從List屏除
                 numbers.RemoveAt(r);
             }
-            // 步湊四 重複六次步湊2.3.
             
-
-
             // 停止畫面
             Console.ReadKey();
         }
